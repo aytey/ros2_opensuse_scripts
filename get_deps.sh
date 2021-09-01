b19ed598121a2dad569d83e65418e71dc124bfda
Andrew V. Jones <andrew.jones@vector.com>
2021-09-01 10:56:21 +0100

    scripts to do a Ros2 build

---
+++ get_deps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eu
+
+sudo zypper install -y git gcc11-c++ cmake libacl-devel tinyxml2-devel opencv-devel patch liblog4cxx-devel python3-qt5-devel asio-devel libeigen3-devel bison libXaw-devel libcurl-devel python3-sip4-devel freetype2-devel libbullet-devel
+sudo update-alternatives --remove-all gcc
+sudo update-alternatives --remove-all g++
+sudo update-alternatives --install /usr/bin/gcc gcc "$(which gcc-11)" 10
+sudo update-alternatives --install /usr/bin/g++ g++ "$(which g++-11)" 10
+
+# EOF
